7650114f5ecc304b278fa3a0180b140ce7bd4e29
David Erikefe <dafenone2006@gmail.com>
2026-05-15 14:55:15 +0100

    Deployment fix: added dynamic port for Render

--- app/app.py
+++ app/app.py
@@ -102,4 +102,6 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
